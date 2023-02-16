5db12ab45b450c708a209fc7a8ee57fdbd8cc53b
warimap <warimapharium@gmail.com>
2023-02-16 01:02:36 -0800

    Task 5. Hello, printf mandatory

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
