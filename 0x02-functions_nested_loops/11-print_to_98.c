c16806ad5515a119f742334cef0440c595d9f6ed
warimap <warimapharium@gmail.com>
2023-02-20 23:58:43 -0800

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints numbers from given n to 98, inorder
+ * separated by a comma, followed by a new line
+ * @n: parameter
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n--);
+		}
+
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n++);
+		}
+		printf("%d\n", n);
+	}
+}
