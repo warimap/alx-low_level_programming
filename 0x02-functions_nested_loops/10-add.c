dce4cf6a318a1e762d5ab4fc6fdd4ea70bcf2e4c
warimap <warimapharium@gmail.com>
2023-02-20 23:57:22 -0800

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two given numbers
+ * @num1: parameter 1
+ * @num2: parameter 2
+ * Return: always 0
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+
+	return (sum);
+}
