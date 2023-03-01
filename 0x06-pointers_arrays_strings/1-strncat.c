66973f3afdbb44d922acd4a06ec7a23c08d9277d
warimap <warimapharium@gmail.com>
2023-03-01 07:13:31 -0800

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,20 @@
-#include <main.h>
- /**
+#include "main.h"
+/**
  * _strncat - concatenates two strings
  * @dest: the destination value
  * @src: the source value
  * @n: the limit of the concatenation
  * Return: a pointer to the resulting string dest
  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
 
-char *_strncat(char *dest, const char *src, size_t n) {
-    size_t dest_len = strlen(dest);
-    size_t i;
+	while (dest[index++])
+		dest_len++;
 
-    for (i = 0; i < n && src[i] != '\0'; i++) {
-        dest[dest_len + i] = src[i];
-    }
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 
-    dest[dest_len + i] = '\0';
-
-    return dest;
+	return (dest);
 }
