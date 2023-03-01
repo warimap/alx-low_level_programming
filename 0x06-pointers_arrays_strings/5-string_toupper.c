054d4138152674640b8f928dfa2907bf121ea2f6
warimap <warimapharium@gmail.com>
2023-03-01 07:24:43 -0800

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,14 +4,14 @@
  * @s: string to be converted to uppercase
  * Return: converted string
  */
-char *string_toupper(char *str)
+char *string_toupper(char *s)
 {
-    char *p = str;
-    while (*p != '\0') {
-        if (*p >= 'a' && *p <= 'z') {
-            *p = (*p - 'a') + 'A';
-        }
-        p++;
-    }
-    return str;
+	int i = 0;
+
+	for (i = 0;  s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+	}
+	return (s);
 }
