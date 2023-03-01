d240fce1042d6708cc6ef0b1888417a0c44bfce7
warimap <warimapharium@gmail.com>
2023-03-01 07:27:01 -0800

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,28 +1,25 @@
 #include "main.h"
 /**
- * leet - encodes a string into 1337
- * @s: input string.
- * Return: the pointer to dest.
+ * *leet - encodes a string into 1337
+ * @s: string to be encoded
+ * Return: encoded string
  */
-char *leet(char *str) {
-    char *leet_chars = "aAeEoOtTlL";
-    char *leet_repl = "44330771";
-    int i, j;
-    for (i = 0; str[i] != '\0'; i++) {
-        for (j = 0; j < strlen(leet_chars); j++) {
-            if (str[i] == leet_chars[j]) {
-                str[i] = leet_repl[j];
-                break;
-            }
-        }
-    }
-    return str;
+char *leet(char *s)
+{
+	int i, n;
+	char letters[] = "aAeEoOtTlL";
+	char numbers[] = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (n = 0; n < 10; n++)
+		{
+			if (s[i] == letters[n])
+			{
+				s[i] = numbers[n];
+			}
+		}
+	}
+	return (s);
 }
 
-int main() {
-    char str[] = "Hello, World!";
-    printf("Original string: %s\n", str);
-    leet(str);
-    printf("Leet string: %s\n", str);
-    return 0;
-}
