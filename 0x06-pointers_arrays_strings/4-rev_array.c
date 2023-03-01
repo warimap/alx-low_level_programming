6fcff5bbc90f62e081c5fbcd3351b04b8fceb24b
warimap <warimapharium@gmail.com>
2023-03-01 07:21:22 -0800

    Task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,10 +7,13 @@
  */
 void reverse_array(int *a, int n)
 {
-    int i, temp;
-    for (i = 0; i < n / 2; i++) {
-        temp = a[i];
-        a[i] = a[n - i - 1];
-        a[n - i - 1] = temp;
-    }
+	int i, temp, end;
+
+	end = n - 1;
+	for (i = 0; i < end; i++, end--)
+	{
+		temp = a[end];
+		a[end] = a[i];
+		a[i] = temp;
+	}
 }
