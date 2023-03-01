019f7999a1e4dfe48703719eb3745463210f1043
warimap <warimapharium@gmail.com>
2023-03-01 00:08:18 -0800

    Task 3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strcmp - Function which compare two strings
+ * @s1: first string
+ * @s2:second string
+ *Return: 0 if s1 and s2 are equals,
+ * another number if not.
+ */
+int _strcmp(char *s1, char *s2)
+{
+    int i = 0;
+
+    while (s1[i] == s2[i])
+    {
+        if (s1[i] == '\0')
+            return (0);
+        i++;
+    }
+
+    return (s1[i] - s2[i]);
+}
