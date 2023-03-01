9442f1affe7be335d20e19439b659cd541768134
warimap <warimapharium@gmail.com>
2023-02-28 23:59:16 -0800

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,20 +1,22 @@
 #include "main.h"
 /**
- * _strncpy - copies a string
- * @dest: destination.
- * @src: source.
- * @n: amount of bytes from src.
- * Return: the pointer to dest.
+ * _strncat - concatenates two strings
+ * @dest: the destination value
+ * @src: the source value
+ * @n: the limit of the concatenation
+ * Return: a pointer to the resulting string dest
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+    int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = '\0';
+    for (i = 0; i < n && src[i] != '\0'; i++) {
+        dest[i] = src[i];
+    }
 
-	return (dest);
+    while (i < n) {
+        dest[i++] = '\0';
+    }
+
+    return dest;
 }
