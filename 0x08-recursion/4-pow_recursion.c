4b44b4682e7ae20ea274438707638e63a872302d
warimap <warimapharium@gmail.com>
2023-03-08 01:23:40 -0800

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _pow_recursion - raises x to the power y
+ * @x: base
+ * @y: power
+ * Return: x power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
