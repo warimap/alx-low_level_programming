ec2b0eb35bea9a197820ff99de64235d561994af
warimap <warimapharium@gmail.com>
2023-03-22 03:13:29 -0700

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,11 +1,15 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 /**
- * op_add - Returns the sum of two numbers
+ * op_add - Returns the sum of two numbers.
  * @a: The first number.
- * @b: The second number
+ * @b: The second number.
  *
  * Return: The sum of a and b.
  */
@@ -28,10 +32,10 @@ int op_sub(int a, int b)
 
 /**
  * op_mul - Returns the product of two numbers.
- * @a: ...
- * @b: ...
+ * @a: The first number.
+ * @b: The second number.
  *
- *  Return: ...
+ * Return: The product of a and b.
  */
 int op_mul(int a, int b)
 {
@@ -39,37 +43,25 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - ...
- * @a: ...
- * @b: ...
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- *  Return: ...
+ * Return: The quotient of a and b.
  */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a / b);
 }
 
 /**
- * op_mod - ...
- * @a: ...
- * @b: ...
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
  *
- * Return: ...
+ * Return: The remainder of the division of a by b.
  */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
 	return (a % b);
 }
